be678753f5684563becb96b2fcf05a92655b1235
Lucio Silva <sandmanlucio@gmail.com>
2016-10-07 20:49:13 -0300

    Inserindo arquivo principal.js

---
+++ principal.js
@@ -0,0 +1,14 @@
+/**
+ * Created by LucioSilva on 07/10/2016.
+ */
+var banners =
+    ["Os melhores do Brasil!", "Qualidade e preço baixo!"];
+var bannerAtual = 0;
+
+function trocaBanner() {
+    bannerAtual = (bannerAtual + 1) % 2;
+    document.querySelector('h2#mensagem').textContent =
+        banners[bannerAtual];
+}
+
+setInterval(trocaBanner, 2000);
